894253a91f59e73a0bd31e3039151cdbade40180
Marcio <marcio.torres@riogrande.ifrs.edu.br>
2025-12-12 11:22:24 -0300

    frontend and database

---
+++ database/schema.sql
@@ -0,0 +1,25 @@
+-- SQL -> DQL, DML, DDL, DCL, ...
+-- DDL
+-- Data
+-- Definition
+-- Language
+-- constraint!
+DROP DATABASE IF EXISTS tadsflix;
+
+CREATE DATABASE tadsflix;
+
+\c tadsflix;
+
+CREATE TYPE GENERO AS ENUM ('DRAMA', 'COMEDIA', 'ACAO');
+
+DROP TABLE IF EXISTS filmes;
+
+CREATE TABLE IF NOT EXISTS filmes (
+    titulo    TEXT    DEFAULT 'Sem título',
+    genero    GENERO  DEFAULT NULL,
+    hype      INTEGER DEFAULT 3,
+    assistido BOOLEAN DEFAULT FALSE
+);
+
+INSERT INTO filmes (genero, hype, titulo, assistido)
+VALUES ('ACAO', 4, 'Velozes e Furiosos 9', FALSE);
